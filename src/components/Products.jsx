4afb03b20668f591f75445c966c9573ef1377b11
MarianaJimGar <98183323+MarianaJimGar@users.noreply.github.com>
2022-11-23 20:54:08 -0600

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { effect } from "vue";
+import { effect } from "react";
 
 const Products = () => {
     const [data, setData] = useState([]);
